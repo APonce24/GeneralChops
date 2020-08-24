e81465cae225ebe640c88c8ba472ea9acca1dcb7
Alex Ponce <Hailthesun24@gmail.com>
2020-08-24 18:22:04 -0500

    seeded db if there is no data.

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
@@ -7,10 +8,12 @@ namespace MvcMovie.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-
+        
         [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "deicimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
