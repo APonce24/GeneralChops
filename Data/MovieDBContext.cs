66898b2deb691149b6129862f7ae8e10bebb67fd
Alex Ponce <Hailthesun24@gmail.com>
2020-08-23 22:09:34 -0500

    Dbcontext create, made Movies controller with crud.

---
+++ Data/MovieDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MovieDB.Data
+{
+    public class MovieDBContext : DbContext
+    {
+        public MovieDBContext (DbContextOptions<MovieDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
